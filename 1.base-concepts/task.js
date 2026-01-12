9a3c759ecf0685a0dfca9f4c51e706ef983b6b5e
yunimaxxik <nicxefonn@yandex.ru>
2026-01-12 19:18:13 +0300

    repair first example

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -5,16 +5,20 @@ function solveEquation(a, b, c) {
 	if (D == 0) {
 		arr.push(-b / (2 * a));
 	} else if (D > 0) {
-		arr.push((-b + Math.sqrt(d)) / (2 * a));
-		arr.push((-b - Math.sqrt(d)) / (2 * a));
+		arr.push((-b + Math.sqrt(D)) / (2 * a));
+		arr.push((-b - Math.sqrt(D)) / (2 * a));
 	}
 	return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-	percent /= 100;
-	let bodyCredit = amount - contribution;
-	let paymant =
-		bodyCredit * (percent + percent / ((1 + percent) ** countMonths - 1));
-	return (paymant * countMonths).toFixed(2);
+	let n = countMonths;
+	let P = percent / 100 / 12;
+	let S = amount - contribution;
+	if (S === 0) {
+		return 0;
+	}
+	let monthlyPayment = S * (P + P / (Math.pow(1 + P, n) - 1));
+	let totalPayment = monthlyPayment * n;
+	return totalPayment.toFixed(2);
 }
