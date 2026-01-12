1489469b3d78f3e60f023cb0e76da8675d128e04
yunimaxxik <nicxefonn@yandex.ru>
2026-01-12 17:27:51 +0300

    Firts Examples

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,20 @@
-"use strict"
+'use strict';
 function solveEquation(a, b, c) {
-  let arr = [];
-  
-  return arr;
+	let arr = [];
+	let D = b ** 2 - 4 * a * c;
+	if (D == 0) {
+		arr.push(-b / (2 * a));
+	} else if (D > 0) {
+		arr.push((-b + Math.sqrt(d)) / (2 * a));
+		arr.push((-b - Math.sqrt(d)) / (2 * a));
+	}
+	return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+	percent /= 100;
+	let bodyCredit = amount - contribution;
+	let paymant =
+		bodyCredit * (percent + percent / ((1 + percent) ** countMonths - 1));
+	return (paymant * countMonths).toFixed(2);
 }
