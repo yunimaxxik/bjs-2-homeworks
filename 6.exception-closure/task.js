ba6fbdc30b1df8180d0423d7a775bb7031fa963f
yunimaxxik <nicxefonn@yandex.ru>
2026-01-30 21:09:46 +0300

    completed exercises

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,1 +1,59 @@
-﻿
+﻿function parseCount(count) {
+	const parsed = Number.parseFloat(count);
+	if (Number.isNaN(parsed)) {
+		throw new Error('Невалидное значение');
+	}
+	return parsed;
+}
+
+function validateCount(count) {
+	try {
+		return parseCount(count);
+	} catch (error) {
+		console.log(error);
+	}
+}
+
+// exercise 2
+
+class Triangle {
+	constructor(a, b, c) {
+		try {
+			if (a <= 0 || b <= 0 || c <= 0) {
+				throw new Error('Стороны должны быть положительными');
+			}
+			if (a + b <= c || a + c <= b || b + c <= a) {
+				throw new Error('Треугольник не существует');
+			}
+			this.a = a;
+			this.b = b;
+			this.c = c;
+		} catch (error) {
+			console.log(error);
+		}
+	}
+	get perimeter() {
+		return this.a + this.b + this.c;
+	}
+	get area() {
+		let p = (this.a + this.b + this.c) / 2;
+		return +Math.sqrt(p * (p - this.a) * (p - this.b) * (p - this.c)).toFixed(
+			3,
+		);
+	}
+}
+
+function getTriangle(a, b, c) {
+	try {
+		return new Triangle(a, b, c);
+	} catch (error) {
+		return {
+			get area() {
+				return 'Ошибка! Треугольник не существует';
+			},
+			get perimeter() {
+				return 'Ошибка! Треугольник не существует';
+			},
+		};
+	}
+}
